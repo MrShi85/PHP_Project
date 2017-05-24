87b70728b6179a0c6f3912d3bde9306c12784a84
Shilkin Sergey <ioda85@mail.ru>
2017-05-24 14:41:52 +0300

    MVC part 1

---
+++ config/routes.php
@@ -0,0 +1,8 @@
+<?php
+
+
+return array (
+    'news' => 'news/index', //actionIndex in NewsController
+    'product' => 'product/list', //actionList in ProductController
+);
+
--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php
 
-echo 1+2;
+//FRONT CONTROLLER
+
+//1. Общие настройки
+
+ini_set('display-errors', 1);
+error_reporting(E_ALL);
+
+//2. Подключение файлов системы
+
+define('ROOT', dirname(__FILE__));
+require_once(ROOT.'/components/Router.php');
+
+//3. Установка соединения с БД
+
+
+//4. Вызов Router
+
+$router = new Router();
+$router->run();
 
